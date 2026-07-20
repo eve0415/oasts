0c2b6e7f4ccca946a3972fe33d88a88731cdb322
eve0415 <40632436+eve0415@users.noreply.github.com>
2026-07-20 09:31:21 +0900

    chore: update oxfmt config

--- packages/oasts/bin/oasts.mjs
+++ packages/oasts/bin/oasts.mjs
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 import { run } from "../src/cli.ts";
 
-const code = await run(process.argv.slice(2), process.cwd(), process.stdout, process.stderr);
-process.exit(code);
+process.exit(await run(process.argv.slice(2), process.cwd(), process.stdout, process.stderr));
